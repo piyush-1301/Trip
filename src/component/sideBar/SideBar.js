7350f5b90cc4640a86696941d6dac7eb787d3417
Piyush Soni <69707904+piyush-1301@users.noreply.github.com>
2021-06-12 20:24:32 +0530

    Css changes

--- src/component/sideBar/SideBar.js
+++ src/component/sideBar/SideBar.js
@@ -3,27 +3,31 @@ import "./SideBar.css";
 import HomeIcon from "@material-ui/icons/Home";
 import DateRangeIcon from "@material-ui/icons/DateRange";
 import GridOnIcon from "@material-ui/icons/GridOn";
-import { Button, Box } from "@material-ui/core";
+import { Button, Paper } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 function SideBar() {
   return (
     <div className="SideBar">
-      <Link to="/">
-        <Button>
-          <HomeIcon />
-        </Button>
-      </Link>
-      <Link to="/info">
-        <Button>
-          <DateRangeIcon />
-        </Button>
-      </Link>
-      <Link to="/table">
-        <Button>
-          <GridOnIcon />
-        </Button>
-      </Link>
+      <Paper elevation={3}>
+        <div className="SideBarDiv">
+          <Link to="/">
+            <Button style={{ padding: "15px" }}>
+              <HomeIcon />
+            </Button>
+          </Link>
+          <Link to="/info">
+            <Button style={{ padding: "15px" }}>
+              <DateRangeIcon />
+            </Button>
+          </Link>
+          <Link to="/table">
+            <Button style={{ padding: "15px" }}>
+              <GridOnIcon />
+            </Button>
+          </Link>
+        </div>
+      </Paper>
     </div>
   );
 }
